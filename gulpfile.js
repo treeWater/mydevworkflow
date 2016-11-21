e53b72d75118a3acf566950155020a1a8f862c4c
Thomas Chang <tianlong8bu@gmail.com>
2016-11-21 12:25:08 +0800

    add coffee scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp'),
 	// gutil logs error
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
-gulp.task('log', function(){
-	gutil.log("mydevworkflow is awesome");
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSources)
+	.pipe(coffee({bare: true})
+		.on('error', gutil.log)
+		)
+	.pipe(gulp.dest('components/scripts/'))
 });
 
 
